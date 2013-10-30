e701f4627fbdfab52a9affc36613776bfc4aacb7
ryuzaki <ryuzaki@longdestiny.dev>
2013-10-30 21:03:21 +0000

    product images added. basket qty input field styled.

--- application/views/basket/list.php
+++ application/views/basket/list.php
@@ -18,7 +18,7 @@
 				echo form_hidden($i.'[rowid]', $items['rowid']); ?>
 			
 				<tr class="tr_bot_border">
-				  <td><?php echo form_input(array('name' => $i.'[qty]', 'value' => $items['qty'], 'maxlength' => '3', 'size' => '5')); ?></td>
+				  <td><?php echo form_input(array('name' => $i.'[qty]', 'value' => $items['qty'], 'maxlength' => '3', 'size' => '5', 'class' => 'qty_field')); ?></td>
 				  <td>
 					<?php echo $items['name']; ?>
 			
