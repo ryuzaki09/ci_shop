fb3bf56a36e823ab33ffcb37f5644a4c1536efeb
ryuzaki <ryuzaki@shoplongdestiny.dev>
2014-04-23 19:16:45 +0100

    updated page content. allowed editing of page content in cms.

--- application/controllers/admin/settings.php
+++ application/controllers/admin/settings.php
@@ -3,47 +3,56 @@
 class Settings extends CI_Controller {
 
     public function __construct(){
-	parent::__construct();
-	$this->load->library("adminpage");
-	$this->load->model("adminmodel");
+		parent::__construct();
+		$this->load->library("adminpage");
+		$this->load->model("adminmodel");
 
     }
     
     public function content(){
 
-	if($this->input->post("save") == "Save"){
-	    $insertdata = array ("page_name" => $this->input->post("page_name", true),
-				"page_content" => $this->input->post("page_content", true)
-				);
+		if($this->input->post("save") == "Save"){
+			$insertdata = array ("page_name" => $this->input->post("page_name", true),
+								"page_content" => $this->input->post("page_content", true)
+					);
 
-	    $result = $this->adminmodel->create_update_page_content($insertdata);
-	    echo ($result) ? "done!" : "failed";
+			$result = $this->adminmodel->create_update_page_content($insertdata);
+			echo ($result) ? "done!" : "failed";
 
-	}
+		}
 
-	//get list of all page Content
-	$data['result'] = $this->adminmodel->getPageContentList();
-	$data['pagetitle'] = "Page Content";
+		//get list of all page Content
+		$data['result'] = $this->adminmodel->getPageContentList();
+		$data['pagetitle'] = "Page Content";
 
-	$this->adminpage->loadpage("admin/settings/page_content", $data);
+		$this->adminpage->loadpage("admin/settings/page_content", $data);
     }
 
     public function edit_page($page_id){
 
-	$data['result'] = $this->adminmodel->getPageContent($page_id);
-	$data['pagetitle'] = "Edit Page";
+		if($this->input->post("save") == "Save"){
+			$updatedata = array ("page_name" => $this->input->post("page_name", true),
+								"page_content" => $this->input->post("page_content", true)
+								);
+			
+			$result = $this->adminmodel->create_update_page_content($updatedata, $page_id);
 
-	$data['js'][] = $this->adminpage->set('js', '/js/ckeditor/ckeditor.js');
-	$this->adminpage->loadpage("admin/settings/page_content", $data);
+		}
+
+		$data['result'] = $this->adminmodel->getPageContent($page_id);
+		$data['pagetitle'] = "Edit Page";
+
+		$data['js'][] = $this->adminpage->set('js', '/js/ckeditor/ckeditor.js');
+		$this->adminpage->loadpage("admin/settings/page_content", $data);
 
     }
 
     public function createPage(){
 
 
-	$data['pagetitle'] = "Create Page";
+		$data['pagetitle'] = "Create Page";
         $data['js'][] = $this->adminpage->set('js', '/js/ckeditor/ckeditor.js'); 
-	$this->adminpage->loadpage("admin/settings/page_content", $data);
+		$this->adminpage->loadpage("admin/settings/page_content", $data);
 
     }
 
--- application/models/adminmodel.php
+++ application/models/adminmodel.php
@@ -2,11 +2,11 @@
 
 class Adminmodel extends Commonmodel {
         
-    function __construct(){
+    public function __construct(){
         parent::__construct();
     }
     
-    function adminlogin($username, $password){        
+    public function adminlogin($username, $password){        
         $this->db->select('id');
         $this->db->where('name', $username);
         $this->db->where('pwd', $password);
@@ -21,167 +21,73 @@ class Adminmodel extends Commonmodel {
         }        
     }
     
-    function retrieve_data($id){
+    public function retrieve_data($id){
         
         $this->db->where('id', $id);
                 
         $result = $this->db->get($this->table['frontpage']);
         
         return ($result->num_rows() > 0)
-        ? $result->row()
-        : false;
+			? $result->row()
+			: false;
         
     }
 	
-	function retrieve_user($id){
+	public function retrieve_user($id){
         
         $this->db->where('uid', $id);
                 
         $result = $this->db->get($this->table['users']);
         
         return ($result->num_rows() > 0)
-        ? $result->row()
-        : false;
+				? $result->row()
+				: false;
         
     }
     
-    function retrieve_fpphotos($id, $foldername=false){
-        $this->db->where('windowID', $id);
-        
-        if($foldername){
-            $this->db->where('foldername', 'thumbs');
-            $result = $this->db->get($this->table['fpphotos']);
-            return ($result->num_rows() > 0)
-                    ? $result->result_array()
-                    : false;
-            
-        } else {
-            $this->db->where('foldername', 'frontpage');
-            
-            $result = $this->db->get($this->table['fpphotos']);
-            
-            return ($result->num_rows()>0)
-                    ? $result->row()
-                    : false;
-        }          
-                
-    }
     
-    function update_fpphotos($id, $imgname){
-        $this->db->where('windowID', $id);
-        $this->db->set('imgname', $imgname);
-        
-        return $this->db->update($this->table['fpphotos']);
-        
-    }
-    
-    function update_window($id, $data){
-        $this->db->where('id', $id);
-        return $this->db->update($this->table['frontpage'], $data);
-    }
-    
-    function delete_window($id){
-        $this->db->where('id', $id);
-        $result = $this->db->get($this->table['frontpage']);
-        
-        if ($result){
-            $result = $result->row();            
-               //delete from thumb folder
-               unlink($_SERVER['DOCUMENT_ROOT'].'/media/images/frontpage/'.$result->image);            
-        }
-        
-        $this->db->where('id', $id);
-        $result2 = $this->db->delete($this->table['frontpage']);       
-        
-        if(mysql_affected_rows() > 0){
-            
-            return ($this->__delete_fpphotos($id))
-                   ? true
-                   : "no_images_to_delete";
-            
-        } else {
-            return false;
-        }
-        
-    }
-    
-    function __delete_fpphotos($id){
-        $this->db->where('windowID', $id);
-        $result = $this->db->get($this->table['fpphotos']);
-        
-        if ($result){
-            $result = $result->result_array();
-            foreach($result AS $img){                
-               //delete from thumb folder
-               unlink($_SERVER['DOCUMENT_ROOT'].'/media/images/frontpage/thumb/'.$img['imgname']);                
-            }
-            $this->db->where('windowID', $id);
-            $result2 = $this->db->delete($this->table['fpphotos']);
-            return (mysql_affected_rows()>0)
-                    ? true
-                    : false;                  
-            
-        } else {
-            return false;
-        }
-    }
-    
-        
-    function changepwd($data, $where){
+    public function changepwd($data, $where){
         $this->db->where('name', $where);
         return $this->db->update($this->table['adminusers'], $data);
         
     }
     
-    function fetch_albums(){
-        $result = $this->db->get($this->table['photoalbum']);
-        return ($result >= 1)
-            ? $result->result_array()
-            : false;
-    }
-    
-    function add_new_album($foldername){
-        $this->db->set('folder_name', $foldername);        
-        return $this->db->insert($this->table['photoalbum']);
-    }
-
     public function create_update_page_content($data, $where=false){
 	
-	if($where){
-	    $this->db->where("id", $where);
-	    $this->db->update("page_content", $data);
-	} else {
-	    $this->db->insert("page_content", $data);
-	}
+		if($where){
+			$this->db->where("id", $where);
+			$this->db->update("page_content", $data);
+		} else {
+			$this->db->insert("page_content", $data);
+		}
 
-	return ($this->db->affected_rows()>0)
-		? true
-		: false;
+		return ($this->db->affected_rows()>0)
+			? true
+			: false;
 
     }
 
     public function getPageContentList(){
 	
-	$result = $this->db->get("page_content");
+		$result = $this->db->get("page_content");
 
-	return ($result->num_rows()>0)
-		? $result->result_array()
-		: false;
+		return ($result->num_rows()>0)
+			? $result->result_array()
+			: false;
 
     }
 
     public function getPageContent($id){
 	
-	$this->db->where("id", $id);
+		$this->db->where("id", $id);
 
-	$result = $this->db->get("page_content");
+		$result = $this->db->get("page_content");
 
-	return ($result->num_rows()>0)
-		? $result->row()
-		: false;
+		return ($result->num_rows()>0)
+			? $result->row()
+			: false;
 
     }
     
     
 }
-?>
