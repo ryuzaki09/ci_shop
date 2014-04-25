67400c608d67dd3e76f80d6b7e1b3e3cf86819f6
ryuzaki <ryuzaki@longdestiny.dev>
2014-04-25 19:12:32 +0100

    styles updated.

--- application/views/basket/confirm.php
+++ application/views/basket/confirm.php
@@ -1,10 +1,13 @@
 <div class="container">
     <div class="basket_container">
-	<div class="basket_title">Confirmation</div>
-	<div class="bottom_space">Name: <?php echo $userdata->firstname." ".$userdata->lastname; ?></div>
-	<div class="bottom_space">Address: <?php echo $userdata->address1."<br />".$userdata->address2; ?></div>
-	<div class="bottom_space">Postcode: <?php echo $userdata->postcode; ?></div>
-	<div class="bottom_space">Email: <?php echo $userdata->email; ?></div>
+	<div class="basket_title">Delivery Address</div>
+	<div class="bot30">
+		<div class="bottom_space">Name: <?php echo $userdata->firstname." ".$userdata->lastname; ?></div>
+		<div class="bottom_space">Address: <?php echo $userdata->address1."<br />".$userdata->address2; ?></div>
+		<div class="bottom_space">Postcode: <?php echo $userdata->postcode; ?></div>
+		<div class="bottom_space">Email: <?php echo $userdata->email; ?></div>
+		<button> <a>Alternative delivery address</a> </button>
+	</div>
 		
 	<?php
 	if($environment){
@@ -24,39 +27,39 @@
 	    $i = 1; 
 			
 	    foreach ($this->cart->contents() as $items): 
-		//hidden form fields to send back to server to process	
-		echo form_hidden($i.'[rowid]', $items['rowid']); 
-		echo form_hidden($i.'[qty]', $items['qty']); 
-		echo form_hidden($i.'[price]', $items['price']); 
-		echo form_hidden($i.'[pid]', $items['id']); 
-		echo form_hidden($i.'[name]', $items['name']); 
-		echo form_hidden($i.'[subtotal]', $items['subtotal']); 
+			//hidden form fields to send back to server to process	
+			echo form_hidden($i.'[rowid]', $items['rowid']); 
+			echo form_hidden($i.'[qty]', $items['qty']); 
+			echo form_hidden($i.'[price]', $items['price']); 
+			echo form_hidden($i.'[pid]', $items['id']); 
+			echo form_hidden($i.'[name]', $items['name']); 
+			echo form_hidden($i.'[subtotal]', $items['subtotal']); 
+					
+			?>
 				
-		?>
-			
-		<tr class="tr_bot_border">
-		    <td><?php echo $items['qty']; ?></td>
-		    <td>
-			<?php echo $items['name']; ?>
-			
-			<?php if ($this->cart->has_options($items['rowid']) == TRUE): ?>
-			
-			    <p>
-			    <?php foreach ($this->cart->product_options($items['rowid']) as $option_name => $option_value): ?>
-			
-				    <strong><?php echo $option_name; ?>:</strong> <?php echo $option_value; ?><br />
-			
-			    <?php endforeach; ?>
-			    </p>
-			
-			<?php endif; ?>
-			
-		    </td>
-		    <td style="text-align:right">&pound;<?php echo $this->cart->format_number($items['price']); ?></td>
-		    <td style="text-align:right">&pound;<?php echo $this->cart->format_number($items['subtotal']); ?></td>
-		</tr>
-			
-		<?php $i++; 
+			<tr class="tr_bot_border">
+				<td><?php echo $items['qty']; ?></td>
+				<td>
+				<?php echo $items['name']; ?>
+				
+				<?php if ($this->cart->has_options($items['rowid']) == TRUE): ?>
+				
+					<p>
+					<?php foreach ($this->cart->product_options($items['rowid']) as $option_name => $option_value): ?>
+				
+							<strong><?php echo $option_name; ?>:</strong> <?php echo $option_value; ?><br />
+				
+					<?php endforeach; ?>
+					</p>
+				
+				<?php endif; ?>
+				
+				</td>
+				<td style="text-align:right">&pound;<?php echo $this->cart->format_number($items['price']); ?></td>
+				<td style="text-align:right">&pound;<?php echo $this->cart->format_number($items['subtotal']); ?></td>
+			</tr>
+				
+			<?php $i++; 
 			
 		endforeach; 
 		?>
@@ -73,41 +76,42 @@
 
 	    <?php
 	    if($environment){
-		?>
-		</form>
-		<?php
-	    } else {
-		// if(!$environment){
-		?>
-			
-		<div class="clearfix">
-		    <div class="go_right">
-			<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-			    <input type="hidden" name="cmd" value="_cart">
-			    <input type="hidden" name="upload" value="1">
-			    <input type="hidden" name="business" value="2XPWA6XZYS7ZJ">
-			    <input type="hidden" name="currency_code" value="GBP">
-			    <input type="hidden" name="lc" value="GB">
-			    <input type="hidden" name="return" value="http://shop.longdestiny.com/">
-			    <?php
-			    $count =1;
-			    foreach ($this->cart->contents() as $items2): 
-					
-				echo "<input type='hidden' name='item_name_".$count."' value='".$items2['name']."' />";
-				echo "<input type='hidden' name='amount_".$count."' value='".$items2['price']."' />"; 
-				echo "<input type='hidden' name='quantity_".$count."' value='".$items2['qty']."' />";
-						
-				$count++;
-			    endforeach;
-			    ?>
-						
-			    <input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynowCC_LG.gif:NonHosted">
-			    <input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal — The safer, easier way to pay online.">
-			    <img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
+			?>
 			</form>
-		    </div>
-		</div>
-	    <?php
+			<?php
+	    } else {
+			// if(!$environment){
+			?>
+				
+			<div class="clearfix">
+				<div class="go_right">
+				<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+					<input type="hidden" name="cmd" value="_cart">
+					<input type="hidden" name="upload" value="1">
+					<input type="hidden" name="business" value="2XPWA6XZYS7ZJ">
+					<input type="hidden" name="currency_code" value="GBP">
+					<input type="hidden" name="lc" value="GB">
+					<input type="hidden" name="return" value="http://shop.longdestiny.com/">
+					<?php
+					$count =1;
+					foreach ($this->cart->contents() as $items2): 
+						
+						echo "<input type='hidden' name='item_name_".$count."' value='".$items2['name']."' />";
+						echo "<input type='hidden' name='amount_".$count."' value='".$items2['price']."' />"; 
+						echo "<input type='hidden' name='quantity_".$count."' value='".$items2['qty']."' />";
+								
+						$count++;
+					endforeach;
+					?>
+							
+					<input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynowCC_LG.gif:NonHosted">
+					<input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_buynowCC_LG.gif" border="0" 
+								name="submit" alt="PayPal — The safer, easier way to pay online.">
+					<img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
+				</form>
+				</div>
+			</div>
+			<?php
 	    }
 	    ?>
 
